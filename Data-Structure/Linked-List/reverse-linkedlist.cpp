e24bd58b837c96e013968ddecf66002cfea12d6f
achalofficial <achal.official@gmail.com>
2020-06-16 09:37:33 +0530

    Auto Commit

--- Data-Structure/Linked-List/reverse-linkedlist.cpp
+++ Data-Structure/Linked-List/reverse-linkedlist.cpp
@@ -10,6 +10,32 @@ class Node {
 
 Node *head = NULL ;
 
+void displayNode (){
+  Node *temp = head ;
+  while (temp->next != NULL){
+    cout << temp->data << "->" ;
+    temp = temp->next;
+  }
+  cout << " NULL";
+}
+
+void appendNode ( int data){
+
+  // Make new Node
+  Node *new_node = new Node();
+  new_node->data = data ;
+  new_node->next = NULL ;
+
+  //Finding the last Node
+  Node *last = head ;
+  while (last->next != NULL){
+    last = last->next ;
+  }
+
+  last->next = new_node ;
+
+}
+
 int main(int argc, char const *argv[])
 {
   
