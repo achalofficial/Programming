96bd687fae2230a3ef3abde13399ef21cf66b52c
achalofficial <achal.official@gmail.com>
2020-06-16 09:21:45 +0530

    Reverse Linked List

---
+++ Data-Structure/Linked-List/reverse-linkedlist.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std ;
+
+class Node {
+  public :
+    int data ;
+    Node *next ;
+} ;
+
+Node *head = NULL ;
+
+int main(int argc, char const *argv[])
+{
+  
+  return 0;
+}
